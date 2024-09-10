36cd6d685186fa13da802006333afb088cf80fb4
Félix MARQUET <fpmovie4@gmail.com>
2024-09-10 15:04:04 +0200

    First window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
+#include <SDL2/SDL.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+    // Initialiser SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "Erreur d'initialisation de SDL: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // Créer une fenêtre
+    SDL_Window* window = SDL_CreateWindow("Fenêtre SDL2",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          800, 600,
+                                          SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "Erreur de création de la fenêtre: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    // Boucle principale
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+    }
+
+    // Détruire la fenêtre et quitter SDL
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
