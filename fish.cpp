c30d13ece98abd6e214fb331b13bf9bf03b446f3
Félix MARQUET <fpmovie4@gmail.com>
2024-09-13 16:15:35 +0200

    Arrow for fish direction

--- fish.cpp
+++ fish.cpp
@@ -20,8 +20,28 @@ void Fish::move() {
     }
 }
 
+void Fish::drawArrow(SDL_Renderer* renderer, int x, int y, int vx, int vy) {
+    const int arrowLength = 30; // Augmenter la longueur de la flèche
+    const int arrowWidth = 10;  // Garder la largeur de la flèche constante
+    SDL_Point points[4];
+
+    if (vx == 0 && vy == 0) return;
+
+    float angle = atan2(vy, vx);
+    points[0] = { x + static_cast<int>(arrowLength * cos(angle)), y + static_cast<int>(arrowLength * sin(angle)) };
+    points[1] = { x + static_cast<int>(arrowWidth * cos(angle + M_PI / 6)), y + static_cast<int>(arrowWidth * sin(angle + M_PI / 6)) };
+    points[2] = { x + static_cast<int>(arrowWidth * cos(angle - M_PI / 6)), y + static_cast<int>(arrowWidth * sin(angle - M_PI / 6)) };
+    points[3] = points[0];
+
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Vert
+    SDL_RenderDrawLines(renderer, points, 4);
+}
+
+
 void Fish::draw(SDL_Renderer* renderer) {
     SDL_Rect rect = { x, y, width, height };
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Rouge
     SDL_RenderFillRect(renderer, &rect);
+
+    drawArrow(renderer, x + width / 2, y + height / 2, vx, vy);
 }
--- fish.h
+++ fish.h
@@ -5,17 +5,20 @@
 #ifndef FISH_H
 #define FISH_H
 #include <SDL2/SDL.h>
+#include <math.h>
 
 class Fish {
 public:
-    int x, y;
-    int vx, vy;
-    int width, height;
-
     Fish(int x, int y, int vx, int vy, int width, int height) : x(x), y(y), vx(vx), vy(vy), width(width), height(height) {}
 
     void move();
     void draw(SDL_Renderer* renderer);
+    void drawArrow(SDL_Renderer* renderer, int x, int y, int vx, int vy);
+
+private:
+    int x, y;
+    int vx, vy;
+    int width, height;
 };
 
 
