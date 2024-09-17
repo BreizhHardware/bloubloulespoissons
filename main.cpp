033f5bd126f6d0a9dc70d6b723f7d2188295cef3
StevSpotted <41762297+StevSpotted@users.noreply.github.com>
2024-09-17 09:34:27 +0200

    clean

--- main.cpp
+++ main.cpp
@@ -12,7 +12,16 @@
 std::mutex mtx;
 std::atomic<bool> running(true);
 
-// Fonction pour dessiner un fond en dégradé
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
+SDL_Texture* fishesTexture = nullptr;
+std::vector<Fish> fishes;
+
+bool initSDL();
+void handleEvents(int& playerX, int& playerY, const int playerSpeed);
+void renderScene(int playerX, int playerY);
+void cleanup();
+
 void drawGradientBackground(SDL_Renderer* renderer, int offsetX, int offsetY) {
     for (int y = 0; y < ENV_HEIGHT; y++) {
         Uint8 blue = static_cast<Uint8>(255 * (1.0 - static_cast<float>(y) / ENV_HEIGHT));
@@ -21,7 +30,6 @@ void drawGradientBackground(SDL_Renderer* renderer, int offsetX, int offsetY) {
     }
 }
 
-// Fonction pour mettre à jour la position des poissons
 void updateFish(std::vector<Fish>& fishes) {
     while (running) {
         std::this_thread::sleep_for(std::chrono::milliseconds(16));
@@ -33,47 +41,10 @@ void updateFish(std::vector<Fish>& fishes) {
 }
 
 int main(int argc, char* argv[]) {
-    // Initialiser SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cerr << "Erreur d'initialisation de SDL: " << SDL_GetError() << std::endl;
+    if (!initSDL()) {
         return 1;
     }
 
-    // Initialiser SDL_image
-    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
-        std::cerr << "Erreur d'initialisation de SDL_image: " << IMG_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
-    }
-
-    // Créer une fenêtre
-    SDL_Window* window = SDL_CreateWindow("BloubBloub les poissons",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          WINDOW_WIDTH, WINDOW_HEIGHT,
-                                          SDL_WINDOW_SHOWN);
-    if (window == nullptr) {
-        std::cerr << "Erreur de création de la fenêtre: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
-    }
-
-    // Créer un renderer
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == nullptr) {
-        std::cerr << "Erreur de création du renderer: " << SDL_GetError() << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
-    std::vector<Fish> fishes;
-    SDL_Surface* fishesSurface = IMG_Load("../img/poasson.png");
-    SDL_Texture* fishesTexture = SDL_CreateTextureFromSurface(renderer, fishesSurface);
-    SDL_FreeSurface(fishesSurface);
-    //for (int i = 0; i < 100; ++i) {
-    //    fishes.emplace_back(rand() % ENV_WIDTH, rand() % ENV_HEIGHT, rand() % 3 - 1, rand() % 3 - 1, 10, 5, renderer, nullptr);
-    //}
     for (int i = 0; i < 900; ++i) {
         fishes.emplace_back(rand() % ENV_WIDTH, rand() % ENV_HEIGHT, rand() % 3 - 1, rand() % 3 - 1, 50, 50, renderer, fishesTexture);
     }
@@ -84,86 +55,127 @@ int main(int argc, char* argv[]) {
     Reef reef(300, 300);
     Kelp kelp(500, 500, 100, 4, 87, 0);
 
-    SDL_Event event;
-
     int playerX = WINDOW_WIDTH / 2;
     int playerY = WINDOW_HEIGHT / 2;
     const int playerSpeed = 5;
 
     while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = false;
-            } else if (event.type == SDL_KEYDOWN) {
-                switch (event.key.keysym.sym) {
-                    case SDLK_z:
-                        playerY -= playerSpeed;
-                        break;
-                    case SDLK_s:
-                        playerY += playerSpeed;
-                        break;
-                    case SDLK_q:
-                        playerX -= playerSpeed;
-                        break;
-                    case SDLK_d:
-                        playerX += playerSpeed;
-                        break;
-                }
-            }
-        }
-
-        // Calculer les offsets du viewport
-        int offsetX = playerX - WINDOW_WIDTH / 2;
-        int offsetY = playerY - WINDOW_HEIGHT / 2;
-
-        // Empêcher le viewport de dépasser les bords de l'environnement
-        if (offsetX < 0) offsetX = 0;
-        if (offsetY < 0) offsetY = 0;
-        if (offsetX > ENV_WIDTH - WINDOW_WIDTH) offsetX = ENV_WIDTH - WINDOW_WIDTH;
-        if (offsetY > ENV_HEIGHT - WINDOW_HEIGHT) offsetY = ENV_HEIGHT - WINDOW_HEIGHT;
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-
-        drawGradientBackground(renderer, offsetX, offsetY);
-
-        rock.draw(renderer);
-        reef.draw(renderer);
-        kelp.draw(renderer);
-
-        std::lock_guard<std::mutex> lock(mtx);
-        for (auto& fish : fishes) {
-            fish.draw(renderer);
-        }
-
-        // Dessiner le joueur
-        SDL_Rect playerRect = { playerX - offsetX, playerY - offsetY, 20, 20 };
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Blanc
-        SDL_RenderFillRect(renderer, &playerRect);
-
-        SDL_RenderPresent(renderer);
+        handleEvents(playerX, playerY, playerSpeed);
+        renderScene(playerX, playerY);
         SDL_Delay(10);
     }
 
-    if(renderer != nullptr){
-            SDL_DestroyRenderer(renderer);
-        }
-
-        if(window != nullptr){
-            SDL_DestroyWindow(window);
-        }
-
     running = false;
     fishThread.join();
-
-    SDL_DestroyTexture(fishesTexture);
-    if (fishesSurface != nullptr) {
-        SDL_FreeSurface(fishesSurface);
-    }
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    IMG_Quit();
-    SDL_Quit();
+    cleanup();
 
     return 0;
+}
+
+bool initSDL() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "Erreur d'initialisation de SDL: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        std::cerr << "Erreur d'initialisation de SDL_image: " << IMG_GetError() << std::endl;
+        SDL_Quit();
+        return false;
+    }
+
+    window = SDL_CreateWindow("BloubBloub les poissons",
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              WINDOW_WIDTH, WINDOW_HEIGHT,
+                              SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "Erreur de création de la fenêtre: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        std::cerr << "Erreur de création du renderer: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return false;
+    }
+
+    SDL_Surface* fishesSurface = IMG_Load("../img/poasson.png");
+    fishesTexture = SDL_CreateTextureFromSurface(renderer, fishesSurface);
+    SDL_FreeSurface(fishesSurface);
+
+    return true;
+}
+
+void handleEvents(int& playerX, int& playerY, const int playerSpeed) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            running = false;
+        } else if (event.type == SDL_KEYDOWN) {
+            switch (event.key.keysym.sym) {
+                case SDLK_z:
+                    playerY -= playerSpeed;
+                    break;
+                case SDLK_s:
+                    playerY += playerSpeed;
+                    break;
+                case SDLK_q:
+                    playerX -= playerSpeed;
+                    break;
+                case SDLK_d:
+                    playerX += playerSpeed;
+                    break;
+            }
+        }
+    }
+}
+
+void renderScene(int playerX, int playerY) {
+    int offsetX = playerX - WINDOW_WIDTH / 2;
+    int offsetY = playerY - WINDOW_HEIGHT / 2;
+
+    if (offsetX < 0) offsetX = 0;
+    if (offsetY < 0) offsetY = 0;
+    if (offsetX > ENV_WIDTH - WINDOW_WIDTH) offsetX = ENV_WIDTH - WINDOW_WIDTH;
+    if (offsetY > ENV_HEIGHT - WINDOW_HEIGHT) offsetY = ENV_HEIGHT - WINDOW_HEIGHT;
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    drawGradientBackground(renderer, offsetX, offsetY);
+
+    Rock rock(100, 100, 50, 255, 0, 0);
+    Reef reef(300, 300);
+    Kelp kelp(500, 500, 100, 4, 87, 0);
+
+    rock.draw(renderer);
+    reef.draw(renderer);
+    kelp.draw(renderer);
+
+    std::lock_guard<std::mutex> lock(mtx);
+    for (auto& fish : fishes) {
+        fish.draw(renderer);
+    }
+
+    SDL_Rect playerRect = { playerX - offsetX, playerY - offsetY, 20, 20 };
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &playerRect);
+
+    SDL_RenderPresent(renderer);
+}
+
+void cleanup() {
+    SDL_DestroyTexture(fishesTexture);
+    if (renderer != nullptr) {
+        SDL_DestroyRenderer(renderer);
+    }
+    if (window != nullptr) {
+        SDL_DestroyWindow(window);
+    }
+    IMG_Quit();
+    SDL_Quit();
 }
