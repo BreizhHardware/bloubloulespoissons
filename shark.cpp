18072b7721b949c5053eecba7dbd2081a2c519c2
Clément Fouché <cfouche3005@gmail.com>
2024-12-09 16:50:36 +0100

    add shark

---
+++ shark.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cfouche on 09/12/24.
+//
+
+#include "shark.h"
---
+++ shark.h
@@ -0,0 +1,12 @@
+//
+// Created by cfouche on 09/12/24.
+//
+
+#ifndef SHARK_H
+#define SHARK_H
+
+class Shark {
+
+};
+
+#endif //SHARK_H
